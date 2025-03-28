e9523f8968c049d5555a56aaba07e336664ba4d6
AndriiPyrohovskyi <cbkgfst@gmail.com>
2025-03-28 23:51:48 +0200

    fix in firebase.ts

--- src/auth/firebase.ts
+++ src/auth/firebase.ts
@@ -1,19 +1,10 @@
 import * as admin from 'firebase-admin';
-import { initializeApp as initClientApp } from 'firebase/app';
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import * as fs from 'fs';
 
-const credentials = JSON.parse(process.env.FIREBASE_CREDENTIALS || '{}');
+const serviceAccount = JSON.parse(
+  fs.readFileSync('/etc/secrets/firebase-service-account.json', 'utf8')
+);
 
 admin.initializeApp({
-  credential: admin.credential.cert(require(process.env.GOOGLE_APPLICATION_CREDENTIALS!)),
+  credential: admin.credential.cert(serviceAccount),
 });
-
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-};
-
-const clientApp = initClientApp(firebaseConfig);
-const clientAuth = getAuth(clientApp);
-
-export { admin, clientAuth, signInWithEmailAndPassword };
