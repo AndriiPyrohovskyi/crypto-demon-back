e08344901db157281b050510f0a1ad048ba1167a
AndriiPyrohovskyi <cbkgfst@gmail.com>
2025-03-28 23:45:18 +0200

    fix in firebase.ts

--- src/auth/firebase.ts
+++ src/auth/firebase.ts
@@ -5,7 +5,7 @@ import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 const credentials = JSON.parse(process.env.FIREBASE_CREDENTIALS || '{}');
 
 admin.initializeApp({
-  credential: admin.credential.applicationDefault(),
+  credential: admin.credential.cert(require(process.env.GOOGLE_APPLICATION_CREDENTIALS!)),
 });
 
 const firebaseConfig = {
