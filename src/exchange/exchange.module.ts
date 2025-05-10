ca81d9b9544b5169893eec5e8fdb027784d0fa6e
MaksymTomash <maks.tomash4588@gmail.com>
2025-05-10 14:17:19 +0300

    small fix

--- src/exchange/exchange.module.ts
+++ src/exchange/exchange.module.ts
@@ -3,9 +3,17 @@ import { ExchangeService } from './exchange.service';
 import { ExchangeController } from './exchange.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Exchange } from './exchange.entity';
+import { UserCurrencyModule } from '../user-currency/user-currency.module';
+import { TransactionModule } from '../transaction/transaction.module';
+import { CurrencyModule } from '../currency/currency.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Exchange])],
+  imports: [
+    TypeOrmModule.forFeature([Exchange]),
+    UserCurrencyModule,
+    TransactionModule,
+    CurrencyModule,
+  ],
   providers: [ExchangeService],
   controllers: [ExchangeController],
   exports: [ExchangeService],
